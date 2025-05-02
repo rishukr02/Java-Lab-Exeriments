56adff44958d3d928c3d3e7293d46ca34065f2c0
rishukr02 <163240401+rishukr02@users.noreply.github.com>
2025-05-02 22:02:55 +0530

    Delete oops exp6/OOPS EXP-6/Exp-6 directory

--- oops exp6/OOPS EXP-6/Exp-6/exp1/myinformation.java
+++
@@ -1,9 +0,0 @@
-public class MyInformation {
-
-    public static void main(String[] args) {
-        System.out.println("Bhargav Das");
-        System.out.println("Chaman Vihar, Phase 2");
-        System.out.println("Dehradun, Uttarakhand, 248001");
-    }
-
-}
--- oops exp6/OOPS EXP-6/Exp-6/src/AdvancedSecurity.java
+++
@@ -1,9 +0,0 @@
-// AdvancedSecurity.java (Will result in a compilation error)
-public class AdvancedSecurity extends SecuritySystem {
-    // Attempt to override authenticateUser()
-    @Override
-    public boolean authenticateUser(String username, String password) {
-        // Some modified authentication logic here
-        return true; // For demonstration purposes
-    }
-}
